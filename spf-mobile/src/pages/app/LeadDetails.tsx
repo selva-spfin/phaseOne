d159aa995a28dd54a3573a23a109ac908f594b21
selvakumarmg <selvaa0115@gmail.com>
2024-02-27 07:24:50 +0530

    lead details completed

--- spf-mobile/src/pages/app/LeadDetails.tsx
+++ spf-mobile/src/pages/app/LeadDetails.tsx
@@ -1,10 +1,11 @@
 import { images } from '../../constants/images';
 import { colors } from '../../constants/colors';
 import React, { useEffect } from 'react';
-import { View, Text, ScrollView, StyleSheet, TouchableWithoutFeedback, Image, FlatList } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, TouchableWithoutFeedback, Image, FlatList, TouchableWithoutFeedbackBase } from 'react-native';
 import { PermissionsAndroid } from 'react-native';
 import RNFetchBlob from 'rn-fetch-blob';
 import { camelCaseToName } from '../../services/helpers/commonHelpers';
+import { useNavigation } from '@react-navigation/native';
 
 interface RouteParams {
   data: Record<string, any>;
@@ -21,6 +22,12 @@ const ASSET_BASE_URL = 'http://d6zg14cua4cuh.cloudfront.net/';
 const LeadDetails: React.FC<Props> = ({ route }) => {
   const { data } = route.params;
   const jsonObject = JSON.parse(data?.assets);
+
+  const navigation = useNavigation();
+
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
 
   useEffect(() => {
     requestStoragePermission();
@@ -61,11 +68,10 @@ const LeadDetails: React.FC<Props> = ({ route }) => {
       });
   };
 
-  const renderGridItem = ({ item }:any) => {
+  const renderGridItem = ({ item }: any) => {
     const URL = ASSET_BASE_URL + item?.asset_path;
     const fileName = data?.First_Name + "-" + item?.asset_name;
-    const type = item?.asset_path.split('.')[1];
-    console.warn(type);
+    const type = item?.asset_path?.split('.')[1];
 
     return (
       <TouchableWithoutFeedback onPress={() => { downloadPdf(URL, fileName) }}>
@@ -79,49 +85,60 @@ const LeadDetails: React.FC<Props> = ({ route }) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <Text style={styles.label}>Name</Text>
-        <Text style={styles.value}>{data?.name}</Text>
+      <View style={{ height: 50, backgroundColor: colors.pink,alignItems:'center', flexDirection:'row',paddingHorizontal:10 }}>
+        <TouchableWithoutFeedback onPress={()=>{handleBackPress()}}>
+        <Image source={images.ic_back} style={styles.backIcon} />
+        </TouchableWithoutFeedback>
+        <Text style={styles.title}>Lead Details</Text>
       </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Email</Text>
-        <Text style={styles.value}>{data?.email}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Loan Amount</Text>
-        <Text style={styles.value}>{data?.loanAmount}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Phone Number</Text>
-        <Text style={styles.value}>{data?.phoneNumber}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Address</Text>
-        <Text style={styles.value}>{data?.address}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Company Name</Text>
-        <Text style={styles.value}>{data?.companyName}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Bank Name</Text>
-        <Text style={styles.value}>{data?.bankName}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Status</Text>
-        <Text style={styles.value}>{data?.status}</Text>
-      </View>
-      <View style={styles.row}>
-        <Text style={styles.label}>Employee Type</Text>
-        <Text style={styles.value}>{data?.Employee_Type}</Text>
-      </View>
-      <FlatList
-        data={jsonObject}
-        renderItem={renderGridItem}
-        keyExtractor={(item, index) => index.toString()}
-        numColumns={4} // Number of columns in the grid
-        contentContainerStyle={styles.gridContainer}
-      />
+      <ScrollView style={{paddingHorizontal:20,paddingTop:10}}>
+        <View style={styles.row}>
+          <Text style={styles.label}>Name</Text>
+          <Text style={styles.value}>{data?.name}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Email</Text>
+          <Text style={styles.value}>{data?.email}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Loan Amount</Text>
+          <Text style={styles.value}>{data?.loanAmount}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Phone Number</Text>
+          <Text style={styles.value}>{data?.phoneNumber}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Address</Text>
+          <Text style={styles.value}>{data?.address}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Company Name</Text>
+          <Text style={styles.value}>{data?.companyName}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Bank Name</Text>
+          <Text style={styles.value}>{data?.bankName}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Status</Text>
+          <Text style={styles.value}>{data?.status}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Employee Type</Text>
+          <Text style={styles.value}>{data?.Employee_Type}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Loan Documents</Text>
+        </View>
+        <FlatList
+          data={jsonObject}
+          renderItem={renderGridItem}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={4}
+          contentContainerStyle={styles.gridContainer}
+        />
+      </ScrollView>
     </View>
   );
 };
@@ -129,7 +146,6 @@ const LeadDetails: React.FC<Props> = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
   },
   section: {
     marginBottom: 20,
@@ -172,7 +188,18 @@ const styles = StyleSheet.create({
   pdfIcon: {
     width: 60,
     height: 60,
-    tintColor: colors.pink,
+    tintColor: '#526D82',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 10,
+    color:'#FFF'
+  },
+  backIcon: {
+    width: 20,
+    height: 20,
+    tintColor: '#FFF',
   },
 });
 
